ec1a39a477c44cf7315705cc86f98adb54e6636f
Andrejs Verza <andrejs.verza@zabbix.com>
2025-10-03 11:41:37 +0300

    03 - Events and Readiness

--- assets/js/class.widget.js
+++ assets/js/class.widget.js
@@ -6,6 +6,31 @@
 class WMRoute extends CWidget {
 
 	/**
+	 * Map object of the Leaflet component.
+	 */
+	#map = null;
+
+	/**
+	 * Resolve as soon as the widget is fully rendered (ready for printing).
+	 *
+	 * Overloads the default implementation of the readiness method.
+	 * Readiness state is crucial for PDF reporting of dashboards.
+	 *
+	 * @returns {Promise<unknown>}
+	 */
+	promiseReady() {
+		return new Promise(resolve => {
+			// Wait unless the map is ready (Leaflet implements its own readiness callback).
+			this.#map.whenReady(() => {
+				// When the map is ready, wait unless the default readiness promise is resolved.
+				super.promiseReady()
+					// Lastly, wait for the final animations to finish.
+					.then(() => setTimeout(resolve, 300));
+			});
+		});
+	}
+
+	/**
 	 * Promise to update the widget.
 	 *
 	 * Overloads the default implementation of the update method.
@@ -25,14 +50,33 @@ class WMRoute extends CWidget {
 			this._body.appendChild(map_wrapper);
 
 			// Focus the map in the center of London.
-			const map = L.map(map_wrapper).setView([51.505, -0.09], 13);
+			this.#map = L.map(map_wrapper).setView([51.505, -0.09], 13);
 
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				attribution: '&copy; OpenStreetMap contributors'
-			}).addTo(map);
+			}).addTo(this.#map);
 		}
 
 		// Return a resolved promise, since the update routine is promise-based.
 		return Promise.resolve();
+	}
+
+	/**
+	 * Intercept the resizing event of the widget container.
+	 */
+	onResize() {
+		if (this.#map !== null) {
+			// Inform the map component that the size of the container has changed.
+			this.#map.invalidateSize();
+		}
+	}
+
+	/**
+	 * Turn off the text mode and use all available space for widget presentation.
+	 *
+	 * @returns {boolean}
+	 */
+	hasPadding() {
+		return false;
 	}
 }
